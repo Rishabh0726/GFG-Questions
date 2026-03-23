9b40763e4cd15314241271dcb63c34d055843ce7
Rishabh Tiwari <153733171+Rishabh0726@users.noreply.github.com>
2026-03-23 17:20:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All Divisors/all-divisors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<Integer> printDivisors(int n) {
+        ArrayList<Integer> list=new ArrayList<>();
+
+        for (int i=1; i*i<=n; i++){
+            if (n%i==0){
+                list.add(i);
+                if (i!=n/i) {
+                    list.add(n/i);
+                }
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
