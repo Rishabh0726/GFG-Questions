561d612278cb95826794dab92b3b559e1d5538b0
Rishabh Tiwari <153733171+Rishabh0726@users.noreply.github.com>
2026-09-10 11:22:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.java
+++ Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public static int countDigits(int n) {
         // Code here
-        int q=n/10;
-        if(q==0)
-        return 1;
-        int cnt=1;
-        while(q!=0){
-            q=q/10;
+        int cnt=0;
+        while(n!=0){
+            n=n/10;
             cnt++;
         }
         return cnt;
