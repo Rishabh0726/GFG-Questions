dab31afc04452ac2f44a0b8069f7dbc1b999ed77
Rishabh Tiwari <153733171+Rishabh0726@users.noreply.github.com>
2026-09-23 15:50:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/All String Permutations in Sorted Order/all-string-permutations-in-sorted-order.java
@@ -0,0 +1,21 @@
+class Solution {
+    private void solve (String s, String output, ArrayList<String> ans){
+        if(s.isEmpty()){
+            ans.add(output);
+            return;
+        }
+        
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            String remString=s.substring(0,i)+s.substring(i+1);
+            solve(remString, output+ch, ans);
+        }
+    }
+    public ArrayList<String> permutation(String s) {
+        // code here
+        ArrayList<String> ans=new ArrayList<>();
+        solve(s, "", ans);
+        Collections.sort(ans);
+        return ans;
+    }
+}
